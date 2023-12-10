a82b8db42a319f2484cacb9dcbe7e2ad704dafc4
Davon Wilson <davontech3@gmail.com>
2023-12-09 21:23:15 -0500

    prework

---
+++ homework.py
@@ -0,0 +1,67 @@
+#Question 1 
+#Write a function to print "hello_USERNAME!" USERNAME is the input of the function. 
+#The first line of the code has been defined as below.
+
+def hello_name(user_name):
+    print(f"Hello {user_name}")
+
+hello_name("Davon")
+
+
+#Question 2
+#Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+
+def first_odds():
+    oddnumberArray = []
+    for i in range(1, 100):
+        if i % 2 != 0:
+            oddnumberArray.append(i)
+        else:
+            pass
+    print(oddnumberArray)
+
+first_odds()
+
+
+#Question 3
+#Please write a Python function, max_num_in_list to return the max number of a given list. 
+#The first line of the code has been defined as below.
+
+def max_num_in_list(a_list):
+    maxnumber = max(a_list)
+    print(maxnumber)
+    return maxnumber
+
+max_num_in_list([1, 9, 6, 3, 20, 37])
+
+
+#Question 4
+# Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100,
+# unless it is also divisible by 400. The return should be boolean Type (true/false).
+
+def is_leap_year(a_year):
+    if (a_year % 4 == 0 and a_year % 100 != 0) or (a_year % 400 == 0):
+        return True
+    else:
+        return False
+        
+print (is_leap_year(2024))
+
+#Question 5
+#Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] 
+# are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+
+def is_consecutive(a_list):
+    sorted_nums = sorted(a_list)  
+    for i in range(len(sorted_nums) - 1):
+        if sorted_nums[i + 1] - sorted_nums[i] != 1:
+            return False
+
+    return True
+
+print(is_consecutive([1, 2, 3, 4, 6]))
+
+        
+        
+        
+          
